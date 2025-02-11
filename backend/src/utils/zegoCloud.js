58c84bc411d6efa54a30f5e0edca90347f5df943
Jaimin Rathod <123061180+JAIMIN-6532@users.noreply.github.com>
2025-02-12 02:09:35 +0530

    frontend/videocall Implemented

--- backend/src/utils/zegoCloud.js
+++ backend/src/utils/zegoCloud.js
@@ -7,14 +7,15 @@ dotenv.config();
 export default async function zegoCloud(req, res, next) {
   try {
     const { appointmentId } = req.body;
+    const aid = appointmentId.toString();
     const appId = Number(process.env.APP_ID);
-    const serverSecret = process.env.APP_SERVERSECRET;
+    const serverSecret = String(process.env.APP_SERVERSECRET);
     const effectiveTimeInSeconds = 3600;
     const payload = "";
     const token = generateToken04(
       appId,
-      appointmentId.toString(),
-      serverSecret.toString(),
+      aid,
+      serverSecret,
       effectiveTimeInSeconds,
       payload
     );
