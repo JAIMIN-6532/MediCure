28ad266ac1c5c84cb15e514abcd3a7d1f0cca499
Jaimin Rathod <123061180+JAIMIN-6532@users.noreply.github.com>
2025-03-22 19:37:18 +0530

    added vercel link

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     res.header(
       'Access-Control-Allow-Origin',
-      'https://medicure-frontend-qii7.onrender.com',
+      'https://medi-cure-tau.vercel.app',
     );
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
